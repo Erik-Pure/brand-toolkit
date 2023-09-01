c249eeace6ba88c9fd4e14616be2daffe40a56db
Erik Långström <erik.langstrom@umu.se>
2023-09-01 14:07:55 +0200

    finished checklist

--- src/pages/BrandChecklist.tsx
+++ src/pages/BrandChecklist.tsx
@@ -24,16 +24,104 @@ const BrandChecklistPage: React.FunctionComponent<IBrandChecklistProps> = (
         </div>
 
         <div className="segment">
-          <label>
-            <h4>Logotyp</h4>
+          <label className="cbLabel">
+          <input type="checkbox" />
+          <div className="customCheckbox"></div>
+          <div className="cbContent">
+            <h4>Logotyp</h4>{" "}
             <p className="ingress">
-              Designa en logotyp som speglar ditt varumärkes personlighet.{" "}
+              - Designa en logotyp som återspeglar varumärkets personlighet.{" "}
             </p>
             <ul>
-              <li>Se till att logotypdesign fungerar för webb och tryck.</li>
-              <li>Testa att den syns bra i små storlekar.</li>
-            </ul>
-            <input type="checkbox" />
+              <li><p>Se till att logotypdesign fungerar för webb och tryck.</p></li>
+              <li><p>Testa att den syns bra i små storlekar.</p></li>
+            </ul>            
+          </div>
+          </label>
+          <label className="cbLabel">
+          <input type="checkbox" />
+          <div className="customCheckbox"></div>
+          <div className="cbContent">
+            <h4>Färgpalett</h4>{" "}
+            <p className="ingress">
+              - Ta fram en enkel med flexibel färgpalett.{" "}
+            </p>
+            <ul>
+              <li><p>Välj 1 huvudfärg, 2 primärfärger, 3-5 komplementfärger, och 2 accentfärger</p></li>              
+            </ul>            
+          </div>
+          </label>
+          <label className="cbLabel">
+          <input type="checkbox" />
+          <div className="customCheckbox"></div>
+          <div className="cbContent">
+            <h4>Typografi</h4>{" "}
+            <p className="ingress">
+              - Välj ett typsnitt som fungerar som en förlängning av din logotyp.{" "}
+            </p>
+            <ul>
+              <li><p>Ta fram ett primärt, sekundärt och ett alternativt typsnitt.</p></li>
+              <li><p>Prova blanda serif och sans serif.</p></li>
+              <li><p>Testa läsbarheten i tryck och på skärm.</p></li>
+            </ul>            
+          </div>
+          </label>
+          <label className="cbLabel">
+          <input type="checkbox" />
+          <div className="customCheckbox"></div>
+          <div className="cbContent">
+            <h4>Fotografier</h4>{" "}
+            <p className="ingress">
+              - Använd en konsekvent och sammanhängande visuell stil.{" "}
+            </p>
+            <ul>
+              <li><p>Se till att bilderna är av hög kvalitet och hög upplösning.</p></li>
+              <li><p>Var uppmärksam på en inkluderande representation.</p></li>
+            </ul>            
+          </div>
+          </label>
+          <label className="cbLabel">
+          <input type="checkbox" />
+          <div className="customCheckbox"></div>
+          <div className="cbContent">
+            <h4>Illustrationer</h4>{" "}
+            <p className="ingress">
+              - Använd illustrationer för att visuellt förbättra, inte överväldiga.{" "}
+            </p>
+            <ul>
+              <li><p>Välj en stil.</p></li>
+              <li><p>Håll det enkelt.</p></li>
+            </ul>            
+          </div>
+          </label>
+          <label className="cbLabel">
+          <input type="checkbox" />
+          <div className="customCheckbox"></div>
+          <div className="cbContent">
+            <h4>Ikoner</h4>{" "}
+            <p className="ingress">
+              - Fokuser på enkelhet och tydlighet.{" "}
+            </p>
+            <ul>
+              <li><p>Dubbelkolla att ikoner återges tydligt i små storlekar.</p></li>
+              <li><p>Se till att bilden är relevant för ändamålet.</p></li>
+            </ul>            
+          </div>
+          </label>
+          <label className="cbLabel">
+          <input type="checkbox" />
+          <div className="customCheckbox"></div>
+          <div className="cbContent">
+            <h4>Datavisualisering</h4>{" "}
+            <p className="ingress">
+              - Designa för enkel förståelse.{" "}
+            </p>
+            <ul>
+              <li><p>Undvik mönster som krockar (använd färg istället).</p></li>
+              <li><p>Överillustrera eller använd inte 3D-diagram.</p></li>
+              <li><p>Sortera/ordna data intuitivt (alfabetisk, stigande eller fallande).</p></li>
+            </ul>            
+          </div>
           </label>
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,33 +10,33 @@ const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
         <div className="segment-header">
           <div>
             <h1>Verktygslåda Grafisk Profil</h1>
-            {/* <h1>Brand Identity Toolkit</h1> */}
+            <Link className="btn" to="/brandaudit">
+              Utvärdera Varumärke
+            </Link>
+            
+            <Link className="btn" to="/competitoraudit">
+              Konkurrensanalys
+            </Link>
+            
+            <Link className="btn" to="/attributespectrum">
+              Utforska kärnvärden
+            </Link>
+            
+            <Link className="btn" to="/brandbrief">
+              Uppdragsbeskrivning
+            </Link>
+            
+            <Link className="btn" to="/brandchecklist">
+              Checklista Grafisk Profil
+            </Link>
             <div className="rebrand-time">
               Är det verkligen dags
               <br /> för en rebrand?
               <img src="/img/arrow.svg" alt="image" />
             </div>
           </div>
-          <div className="testing">
-            <Link className="btn" to="/brandaudit">
-              Utvärdera Varumärke
-            </Link>
-            <br />
-            <Link className="btn" to="/competitoraudit">
-              Konkurrensanalys
-            </Link>
-            <br />
-            <Link className="btn" to="/attributespectrum">
-              Utforska kärnvärden
-            </Link>
-            <br />
-            <Link className="btn" to="/brandbrief">
-              Uppdragsbeskrivning
-            </Link>
-            <br />
-            <Link className="btn" to="/brandchecklist">
-              Checklista Grafisk Profil
-            </Link>
+          <div>
+            
           </div>
         </div>
         <div>
